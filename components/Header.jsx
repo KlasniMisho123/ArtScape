d0421e9dc838588e2858e90b0e04495d78d1eb48
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-13 19:25:53 +0400

    header commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="p-2">
-      <div>
-        <img src='/artScapeLogo.png'></img>
-        <h3> ArtScape </h3>
+    <header className="p-2 border">
+      <div className='flex row'>
+        <img src='/artScapeLogo.png' className='h-[50px]'/>
+        <h3 className='text-[30px]'> ArtScape </h3>
       </div>
       <div></div>
       <div></div>
