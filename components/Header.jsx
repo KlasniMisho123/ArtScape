f23898a142baef7279bac6c8c507d6d3c9c783d7
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-14 02:02:23 +0400

    header commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="p-2 border">
+    <header className="py-2 px-6 border w-full flex justify-between ">
       <div className='flex row'>
         <img src='/artScapeLogo.png' className='h-[50px]'/>
-        <h3 className='text-[30px]'> ArtScape </h3>
+        <h3 className='text-[30px]'> <span>Art</span><span className='text-indigo-500 '>Scape</span> </h3>
       </div>
-      <div></div>
-      <div></div>
+      
+      <div className='flex p-2 gap-6 justify-between  min-w-[500px]'>
+        <button> Gallery </button>
+        <button> Explore </button>
+        <button> Support </button>
+        <div className="w-16 h-16 overflow-hidden rounded-full">
+          <img
+          className='w-full h-full object-cover' 
+          src='ProfilePicDemo.gif'
+          alt='Profile Picture Animation' />
+        </div>
+      </div>
+      
     </header>
   );
 }
