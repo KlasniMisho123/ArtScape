93fac86b6ea35196cec0593945671c18397b89cf
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-14 23:41:11 +0400

    Hero.jsx commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import NavElement from './NavElement';
 import { Lobster, Pacifico, Poppins } from 'next/font/google';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['500'] });
 const pacifico = Pacifico({ subsets: ['latin'], weight: ['400'] });
@@ -13,15 +14,16 @@ const [isAuthenticated, setIsAuthenticated ] = useState(false)
 
   return (
     <header className="py-2 px-6 border w-full flex justify-between ">
-      <div className='flex row'>
+      <Link href='/'
+        className='flex row'>
         <img src='/artScapeLogo.png' className='h-[50px]'/>
         <h3 className='text-[30px]'> <span className={'titleGradientPurple ' + pacifico.className}>Art</span><span className={'text-indigo-500 titleGradientRed ' + poppins.className}>Scape</span> </h3>
-      </div>
+      </Link>
       
       <nav className='flex p-2 gap-2 justify-evenly  min-w-[500px]'>
         <NavElement title="Gallery" link="/gallery" />  
-        <NavElement title="Support" link="/Support" />  
-        <NavElement title="Explore" link="/Explore" icon={<i className="fa-regular fa-compass explore-icon "></i>} />  
+        <NavElement title="Support" link="/support" />  
+        <NavElement title="Explore" link="/explore" icon={<i className="fa-regular fa-compass explore-icon "></i>} />  
       </nav>
         {isAuthenticated? (<div className=" p-0 w-12 h-12 overflow-hidden rounded-full">
           <img
