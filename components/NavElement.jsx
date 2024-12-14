7fd8e3a0fd49634f8d31e3de51cdfd8fcbe181e7
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-14 17:40:57 +0400

    basic hover,shadow commit

--- components/NavElement.jsx
+++ components/NavElement.jsx
@@ -4,8 +4,15 @@ import Link from 'next/link'
 export default function NavElement(props) {
     const { title, link, icon } = props;
   return (
-    <button>
-        <Link href={link} className='flex item-center gap-2 '> <span> {title} </span>  {icon? (<span style={{fontSize:"20px"}}> {icon} </span>) : null} </Link> 
+    <button className='shadow-none hover:shadow-lg transition-shadow ' >
+        <Link href={link} className='flex items-center gap-2'>
+    <span>{title}</span>
+    {icon ? (
+      <span className="text-red-600 text-[20px]">
+        {icon}
+      </span>
+    ) : null}
+  </Link>
     </button>
   )
 }
