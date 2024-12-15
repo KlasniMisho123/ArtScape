30e7e7ac10e7edb09aa779f999039ed8aa3b818d
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-15 17:06:03 +0400

    art Commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className='flex gap-6 bg-gray-900 text-white h-[400px]'>
+        <div className='flex left-intro-div'>
+        <img className="h-[200px] w-[200px] object-cover left one" src='introArt/Guernica.png'/>
+        <img className="h-[200px] w-[200px] object-cover left two" src='introArt/Mona-Lisa.png'/>
+        <img className="h-[200px] w-[200px] object-cover left three" src='introArt/The Kiss.png'/>
+        <img className="h-[200px] w-[200px] object-cover left four" src='introArt/The Last Suppe.png'/>
+        </div>
+
+        <div className='flex right-intro-div'>
+        <img className="h-[200px] w-[200px] object-cover right one " src='introArt/Van-Gogh-Starry-Night.png'/>
+        <img className="h-[200px] w-[200px] object-cover right two" src='introArt/The Night Watch (Nachtwacht).png'/>
+        <img className="h-[200px] w-[200px] object-cover right three" src='introArt/The Persistence.png'/>
+        <img className="h-[200px] w-[200px] object-cover right four" src='introArt/The Scream.png'/>
+        </div>
+    </div>
+
   )
 }
