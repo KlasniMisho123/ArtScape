55d5819e39f90e612d53f6609117d435739da01a
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-18 20:08:33 +0400

    set modal

--- components/AuthenticationForm.jsx
+++ components/AuthenticationForm.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function AuthenticationForm() {
   return (
-    <button className='w-full h-full '>
-      <div className='w-full h-full bg-green-300 flex justify-center absolute'>
-        AuthenticationForms
+    <button className='w-[100%] h-full bg-[rgba(255,255,255,0.5)] flex justify-center absolute z-50 flex justify-center '>
+      <div className='bg-red-200 p-[70px] flex flex-col gap-4 justify-center mt-[80px]'>
+        <h2>Login to website</h2> 
+        <input placeholder='username'/>
+        <input placeholder='password'/>
+        <button> Log in </button>
       </div>
     </button>
   )
