cbbc0497d426e98dfa874cda05c7f0328a73db20
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-18 18:18:23 +0400

    AuthCommit

--- components/AuthenticationForm.jsx
+++ components/AuthenticationForm.jsx
@@ -4,7 +4,7 @@ export default function AuthenticationForm() {
   return (
     <button className='w-full h-full '>
       <div className='w-full h-full bg-green-300 flex justify-center absolute'>
-        AuthenticationForm
+        AuthenticationForms
       </div>
     </button>
   )
