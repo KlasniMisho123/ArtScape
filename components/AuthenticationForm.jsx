7d18c351bd1620d570da25a96a3e9e9c602f8a98
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-19 18:30:42 +0400

    login Form

--- components/AuthenticationForm.jsx
+++ components/AuthenticationForm.jsx
@@ -8,32 +8,65 @@ export default function AuthenticationForm(props) {
   const  { setAuthenticatingActive } = props
 
   const [isRegistered, setIsRegistered ] = useState(true)
+  const [username, setUsername ] = useState("")
+  const [email, setEmail ] = useState("")
+  const [password, setPassword] = useState("")
 
-  function HandleAuthType() {
+  function handleAuthType() {
     console.log("isRegistered: ", isRegistered)
     setIsRegistered(!isRegistered)
   }
 
+  async function handleSubmit() {
+    try {
+      setUsername("")
+      setEmail("")
+      setPassword("")
+    } catch(err) {
+      console.log(err.message)
+    } finally {
+      
+    }
+  }
 
   return (
     <div className='absolute z-50 w-[100%] h-full'>
         <button className='w-full h-[150%] bg-gray-900 bg-opacity-60 flex justify-center absolute z-5 flex justify-center'
         onClick={() => {setAuthenticatingActive(false)}} />
 
-        <div className='bg-white p-[60px] px-[80px] mt-[5%] w-fit mx-auto relative z-5 flex flex-col gap-[40px] rounded-[25px]'>
-            <h2 className={'text-[32px] weight-bolder ' + poppins.className}>{isRegistered ? "Access Your Account" : "Register To Our Platform"}</h2>
+        <div className='bg-white p-[60px] px-[80px] min-w-[450px] mt-[5%] w-fit mx-auto relative z-5 flex flex-col gap-[40px] rounded-[25px]'>
+            <h2 className={'text-[32px] weight-bolder ' + poppins.className}>{isRegistered ? "Login to website" : "Registration from"}</h2>
             <div className='flex flex-col gap-4 '>
               <input
                 placeholder='Username'
-                className='p-2 border rounded-xl outline-none' />
+                className='p-2 border rounded-xl outline-none' 
+                value={username}
+                onChange={(e)=>{
+                  setUsername(e.target.value)
+                }}
+                />
+              <input
+                placeholder='Email'
+                className='p-2 border rounded-xl outline-none' 
+                value={email}
+                onChange={(e)=>{
+                  setEmail(e.target.value)
+                }}
+                />
               <input
                 placeholder='Password'
                 className='p-2 border rounded-xl outline-none'
-                type='password' />
-              <button className='bg-blue-400 text-white py-2 px-4 rounded-lg hover:opacity-90'>
-                Log in
+                type='password'
+                value={password}
+                onChange={(e)=>{
+                  setPassword(e.target.value)
+                }}
+                 />
+              <button className='bg-blue-400 text-white py-2 px-4 rounded-lg hover:opacity-90'
+               onClick={handleSubmit} >
+                {isRegistered ?  "Log In": "Sign up" }
               </button>
-              <p>Don’t have an account? <button onClick={HandleAuthType} className="text-blue-500" >Sign up</button></p>
+              <p> {isRegistered ? "Don’t have an account?" : "Already have an account?"}  <button onClick={handleAuthType} className="text-blue-500" >{isRegistered ? "Sign up" : "Log In"}</button></p>
             </div>
         </div>
     </div>
