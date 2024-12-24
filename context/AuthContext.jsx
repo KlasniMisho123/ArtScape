427d2aaa3078db1ab3f941d6d6ad94c08a0242f5
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-24 21:24:50 +0400

    firebase auth commit

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,18 +1,38 @@
 'use client'
- import React, { useContext , useEffect, useState } from 'react'
- 
- const AuthContext = React.createContext()
 
+import { auth, db } from "../firebase"
+import React, { useContext , useEffect, useState } from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth"
 
- export function useAuth() {
+const AuthContext = React.createContext()
+
+export function useAuth() {
     return useContext(AuthContext)
- }
+}
 
  export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState(null)
     const [authenticatingActive, setAuthenticatingActive] = useState(true)
-    const value = 
-    {
+    const [userDataObj, setUserDataObj] = useState(0)
+    setCurrentUser
+    
+    function signup(email, password) {
+        //username?
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logout() {
+        setUserDataObj(null)
+        setCurrentUser(null)
+        return signOut(auth)
+    }
+
+    const value = {
+        currentUser
     }
 
     return (
