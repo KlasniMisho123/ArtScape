182dbc6b6a33aede9f1b448599c8559055eb87ec
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-24 21:06:25 +0400

    Firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAtH_9HokvAX5gIYtCZkenKqHfRvFlfw9I",
+//   authDomain: "artscape-b8c7b.firebaseapp.com",
+//   projectId: "artscape-b8c7b",
+//   storageBucket: "artscape-b8c7b.firebasestorage.app",
+//   messagingSenderId: "63829047262",
+//   appId: "1:63829047262:web:7090f9e39a8ac283524f4f",
+//   measurementId: "G-6D1XR3CCDX"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyAtH_9HokvAX5gIYtCZkenKqHfRvFlfw9I",
-  authDomain: "artscape-b8c7b.firebaseapp.com",
-  projectId: "artscape-b8c7b",
-  storageBucket: "artscape-b8c7b.firebasestorage.app",
-  messagingSenderId: "63829047262",
-  appId: "1:63829047262:web:7090f9e39a8ac283524f4f",
-  measurementId: "G-6D1XR3CCDX"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_PROJECT_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
