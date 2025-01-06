ee062c16351b3facf8ee6c6fca45368f8427d987
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-07 02:58:51 +0400

    Dark/Light mode button

--- components/ThemeModeButton.jsx
+++ components/ThemeModeButton.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import sunPng from "../public/sun.png"
+import Image from "next/image";
 
 export default function ThemeModeButton(props) {
   const { isLightMode, setIsLightMode } = props
@@ -9,8 +11,14 @@ export default function ThemeModeButton(props) {
   }
   
   return (
-    <button onClick={handleThmeChange}>
-      <div>ThemeModeButton</div>
-    </button>
+    <div className='flex flex-row justify-center'>
+      {/* flex-row-reverse ---> animation */}
+      <button onClick={handleThmeChange} className={`flex justify-between items-center border-2 w-auto rounded-2xl px-2 py-1  w-[50%] ` + (isLightMode?`border-gray bg-white `: `border-gray-100 bg-gray-400 flex-row-reverse `)}>
+          <div>
+            <Image src={sunPng} alt="sun" width={25} height={25} />
+          </div>
+          <i className={"fa-regular fa-circle " + " text-2xl text-[#FFB72B] bg-[#FFD93D] rounded-2xl "}></i>
+      </button>
+    </div>
   )
 }
