e3d133ca59a024b431d186eb102a9313acc0478c
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-07 15:51:11 +0400

    light/dark mode btn commit

--- components/ThemeModeButton.jsx
+++ components/ThemeModeButton.jsx
@@ -12,16 +12,18 @@ export default function ThemeModeButton(props) {
   }
   
   return (
-    <div className='flex flex-row justify-center h-[36.2px]'>
-      {/* flex-row-reverse ---> animation */}
-      <button onClick={handleThmeChange} className={`flex justify-between items-center border-2 w-auto rounded-2xl px-2 py-1  w-[55%] ` + (isLightMode?`border-gray bg-white `: `border-gray-100 bg-gray-700 flex-row-reverse `)}>
+    <div className='flex flex-row justify-center h-[36.2px] overflow-hidden '>
+      <button onClick={handleThmeChange} className={`flex justify-between items-center border-2 w-auto rounded-2xl px-2 py-1 w-[100px] gap-[30px] ` + (isLightMode?`border-gray bg-white `: `border-gray-100 bg-gray-700 `)}>
+        <div className={isLightMode?`sun-div`:`moon-div`}>
           {isLightMode?(
-            <Image src={isLightMode?(sunPng):(moonPng)} alt={isLightMode?"sun":"moon"} width={25} height={25} />
+            <Image src={sunPng} alt="sun" width={25} height={25} className='sun-icon'/>
           ):(
-            <i className="fa-solid fa-moon text-[24px] text-white rotate-[-15deg] "></i>
+            <i className="fa-solid fa-moon text-[24px] text-white moon-icon"></i>
           )}
+        </div>
           
-          <i className={`fa-regular fa-circle text-2xl  rounded-2xl ` + (isLightMode?` text-[#FFB72B] bg-[#FFD93D] `:`text-white bg-gray-700 `)}></i>
+          
+          <i className={`fa-regular fa-circle text-2xl  rounded-2xl ` + (isLightMode?` text-[#FFB72B] bg-[#FFD93D] sun-circle `:`text-white bg-gray-600 moon-circle `)}></i>
       </button>
     </div>
   )
