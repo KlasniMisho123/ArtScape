8fb0e8e56144697e615f800c95b13ab3f0a7b192
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-09 13:58:27 +0400

    profile commit

--- components/ProfileDashboard.jsx
+++ components/ProfileDashboard.jsx
@@ -1,26 +1,38 @@
+import { Open_Sans, Pacifico, Poppins } from 'next/font/google';
 import React from 'react'
 
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['500'] });
+const pacifico = Pacifico({ subsets: ['latin'], weight: ['400'] });
+const openSans = Open_Sans({ subsets: ['latin'], weight: ['400'] })
+
 export default function ProfileDashboard() {
+
+  const gridDefaultCss = ("flex flex-col bg-white p-[30px] rounded-lg shadow-lg ")
+  
+
   return (
-    <div className='flex mt-[30px] w-[80%] mx-auto gap-[300px]'>
-        <div className='flex flex-col '>
-            <button className="flex flex-col bg-red-500 overflow-hidden rounded-full border-2 border-gray-600  h-300 w-300 "> 
+    <div className='flex mt-[30px] p-[40px] w-[75%] mx-auto gap-[100px] bg-gray-100 rounded-lg '>
+        <div className={gridDefaultCss}>
+            <button className="flex flex-col overflow-hidden rounded-full border-2 border-gray-300 h-300 w-300 "> 
                     <img
                     className="object-cover"
-                    src="/ProfilePicDemo.jpg"
+                    src="https://avatars.githubusercontent.com/u/117183990?s=400&u=dbcd799397eb331732f5e0be39ee6cfd1a00f70f&v=4"
                     alt="Profile Picture Animation"
                     height={250}
                     width={250}
                     />
             </button>
-            <div className='flex flex-col items-center mt-4 gap-4 '>
-                <h3 className=''>KLASNIMISHO123</h3>
-                <button className='border py-1 w-full rounded bg-gray-100 '> Edit Profile </button>
-            </div>
+              <div className={`flex flex-col gap-2 py-4`}>
+                  <h3 className={' ' + openSans.className}>KLASNIMISHO123</h3>
+                  <div className='mx-auto '> About Me </div>
+                  <div> ABOUT ME CHART </div>
+              </div>
+                  <button className='border py-1 w-full rounded bg-gray-100 '> Edit Profile </button>
         </div>
-        <div className='flex flex-col border border-black'>
-            <div>Dashboard 1</div>
-            <div>Dashboard 2</div>
+        <div className="flex flex-col gap-[30px] ">
+            <div className={gridDefaultCss + ""}>Dashboard 1</div>
+            <div className={gridDefaultCss + ""}>Dashboard 2</div>
         </div>
     </div>
   )
