c007efceef8c6ed46f72db68f08f376f75cce433
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-13 15:47:06 +0400

    profile Commits

--- components/ProfileDashboard.jsx
+++ components/ProfileDashboard.jsx
@@ -1,13 +1,13 @@
 import { Open_Sans, Pacifico, Poppins } from 'next/font/google';
 import React from 'react'
 import { artworks } from "../app/demoData";
+import Link from 'next/link';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['500'] });
 const pacifico = Pacifico({ subsets: ['latin'], weight: ['400'] });
 const openSans = Open_Sans({ subsets: ['latin'], weight: ['400'] })
 
 export default function ProfileDashboard() {
-
   const gridDefaultCss = ("flex flex-col bg-white p-[30px] rounded-lg shadow-lg ")
 
   return (
@@ -39,12 +39,27 @@ export default function ProfileDashboard() {
                     </ul>
                   </div>
               </div>
-                  <button className='border-2 p-1 '> <i className="fa-solid fa-gear"></i> Edit Profile </button>
+                  <Link href={"/accountmanegement"} className='border-2 p-1 px-2 ' > <i className="fa-solid fa-gear"></i> Edit Profile </Link>
         </div>
-        <div className="flex flex-col gap-[30px] ">
+        <div className="flex flex-col gap-[30px] max-w-[960px] ">
         <div className={gridDefaultCss + ""}>
-          <div className='flex items-center justify-between '>
-           <p> About Klasnimisho123 </p> <button title='Edit' className="fa-regular fa-pen-to-square hover:text-gray-500 "></button>
+          <div className='flex flex-col gap-8 '>
+            <div className='flex items-center justify-between'>
+              <p> About Klasnimisho123 </p> <button title='Edit' className="fa-regular fa-pen-to-square hover:text-gray-500 "></button>
+            </div>
+           
+           <div className='px-1 '>
+                Hi there! I'm a passionate artist who finds joy in bringing imagination to 
+                life through colors and creativity. My art is inspired by the beauty of nature, 
+                emotions, and the stories that connect us all.
+                I specialize in realistic portraits and surreal landscapes,
+                blending traditional techniques with a contemporary touch. Every piece I create tells a story
+                , and my goal is to evoke emotions and spark conversations through my work.
+                When I'm not painting, you’ll find me 
+                exploring art galleries, experimenting with new techniques, or simply soaking in the beauty
+                of the world around me. Thank you for stopping by, and I hope my art resonates with you!
+            </div>
+
           </div>
         </div>
             <div className={`${gridDefaultCss}  py-[30px] bg-red-300 `}>
