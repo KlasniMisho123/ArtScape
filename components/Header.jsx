25b5ebf0920552d75c737e0c66b531f1bd4bf345
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-08 16:25:20 +0400

    theme adjust

--- components/Header.jsx
+++ components/Header.jsx
@@ -69,7 +69,7 @@ useEffect(() => {
                     </div>
                   </button>
               </div>
-              {accountNav? (<div className="border-2 border-t-0 bg-white flex flex-col z-10 pt-4 gap-2 py-2 px-1 rounded  rounded-tl-none rounded-tr-none  ">
+              {accountNav? (<div className="border-2 border-t-0 bg-white flex flex-col z-10 pt-4 gap-2 py-2 px-1 rounded  rounded-tl-none rounded-tr-none text-black ">
 
                 <ThemeModeButton />
                 <NavElement title="My Profile" link={`/profile`} profileType />
