75b156f465e05db84e5d3bfd2e56a7ad2e5a12e6
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-19 19:01:19 +0400

    ManegementSec commit

--- components/AccManegement.jsx
+++ components/AccManegement.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
+import AccManegmentSec from './AccManegmentSec'
 
 export default function AccManegement() {
   return (
     <div className='flex flex-col h-[100vh] w-[90%] mx-auto my-[5%] '>
         <div className='flex'>
         <div>AccManSideNav</div>
-            <div className='flex justify-between mt-[60px]'>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-            </div>
+            <AccManegmentSec />
         </div>
     </div>
   )
---
+++ components/AccManegmentSec.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AccManegmentSec() {
+  return (
+    <div className='flex justify-between mt-[60px]'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+  )
+}
