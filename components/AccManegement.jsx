b025c8148c3dda19e7b667f2a4a94fe577e566ac
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-19 19:20:07 +0400

    management commit

--- components/AccManegement.jsx
+++ components/AccManegement.jsx
@@ -3,9 +3,12 @@ import AccManegmentSec from './AccManegmentSec'
 
 export default function AccManegement() {
   return (
-    <div className='flex flex-col h-[100vh] w-[90%] mx-auto my-[5%] '>
+    <div className='flex flex-col h-[100vh] w-[90%] mx-auto my-[5%] p-4 bg-opacity-10 bg-gray-800 '>
         <div className='flex'>
-        <div>AccManSideNav</div>
+        <div className='flex flex-col' >
+          <section> Account Details</section>
+          <section> Profile Edit </section>
+        </div>
             <AccManegmentSec />
         </div>
     </div>
