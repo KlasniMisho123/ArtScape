b7ef719d6c70f33f413b0eb7d98b1b8b250ff2f9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-20 21:32:16 +0400

    managementbar commit

--- components/AccManegement.jsx
+++ components/AccManegement.jsx
@@ -3,11 +3,13 @@ import AccManegmentSec from './AccManegmentSec'
 
 export default function AccManegement() {
   return (
-    <div className='flex flex-col h-[100vh] w-[90%] mx-auto my-[5%] p-4 bg-opacity-10 bg-gray-800 '>
-        <div className='flex'>
-        <div className='flex flex-col' >
-          <section> Account Details</section>
-          <section> Profile Edit </section>
+    <div className='flex flex-col h-[100vh] w-[90%] mx-auto my-[5%] '>
+        <div className='flex gap-10'>
+        <div className='flex flex-col gap-4 p-2 bg-gray-800 text-white '>
+          <section className='accManegementBar'> General</section>
+          <section className='accManegementBar'> Account Details</section>
+          <section className='accManegementBar'> Avatar Edit </section>
+          <section className='accManegementBar'> Mini Profile </section>
         </div>
             <AccManegmentSec />
         </div>
