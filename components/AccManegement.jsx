b7d491b1c0908e3a5578761dd67cf4b873818f3f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-20 21:59:22 +0400

    managementbar commit

--- components/AccManegement.jsx
+++ components/AccManegement.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import AccManegmentSec from './AccManegmentSec'
 
 export default function AccManegement() {
+  const [currentEditSec, setCurrentEditSec] = useState("general")
+
   return (
     <div className='flex flex-col h-[100vh] w-[90%] mx-auto my-[5%] '>
         <div className='flex gap-10'>
         <div className='flex flex-col gap-4 p-2 bg-gray-800 text-white '>
-          <section className='accManegementBar'> General</section>
-          <section className='accManegementBar'> Account Details</section>
-          <section className='accManegementBar'> Avatar Edit </section>
-          <section className='accManegementBar'> Mini Profile </section>
+          <section className='accManegementBar' onClick={()=>{
+            setCurrentEditSec("General")
+          }}> General</section>
+          <section className='accManegementBar' onClick={()=>{
+            setCurrentEditSec("Account Details")
+          }}> Account Details</section>
+          <section className='accManegementBar' onClick={()=>{
+            setCurrentEditSec("Avatar Edit")
+          }}> Avatar Edit </section>
+          <section className='accManegementBar' onClick={()=>{
+            setCurrentEditSec("Mini Profile")
+          }}> Mini Profile </section>
         </div>
+        {currentEditSec}
             <AccManegmentSec />
         </div>
     </div>
