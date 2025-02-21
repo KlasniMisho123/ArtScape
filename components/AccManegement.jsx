e6c29c4a772fe42a66bd104f9683024708ee404f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-21 17:18:47 +0400

    editProfileDetails commit

--- components/AccManegement.jsx
+++ components/AccManegement.jsx
@@ -9,16 +9,16 @@ export default function AccManegement() {
     <div className='flex flex-col h-[100vh] w-[80%] mx-auto my-[3%] bg-gray-200 p-[2%] '>
         <div className='flex gap-2'>
           <div className='flex flex-col gap-4 py-2 px-1 bg-[#1E3E62] text-white h-fit'>
-            <section className='accManegementBar' onClick={()=>{
+            <section className={'accManegementBar ' + (currentEditSec === "General"? "activeAccManegementBar" : "")} onClick={()=>{
               setCurrentEditSec("General")
             }}> General</section>
-            <section className='accManegementBar' onClick={()=>{
+            <section className={'accManegementBar ' + (currentEditSec === "Account Details"? "activeAccManegementBar" : "")} onClick={()=>{
               setCurrentEditSec("Account Details")
             }}> Account Details</section>
-            <section className='accManegementBar' onClick={()=>{
+            <section className={'accManegementBar ' + (currentEditSec === "Avatar Edit"? "activeAccManegementBar" : "")} onClick={()=>{
               setCurrentEditSec("Avatar Edit")
             }}> Avatar Edit </section>
-            <section className='accManegementBar' onClick={()=>{
+            <section className={'accManegementBar ' + (currentEditSec === "Mini Profile"? "activeAccManegementBar" : "")} onClick={()=>{
               setCurrentEditSec("Mini Profile")
             }}> Mini Profile </section>
         </div>
