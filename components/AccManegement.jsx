594099d48d81019c6b27aaa231147ad3bf51077d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-21 17:45:19 +0400

    editProfileDetails commit

--- components/AccManegement.jsx
+++ components/AccManegement.jsx
@@ -1,14 +1,15 @@
 'use client'
 import React, { useState } from 'react'
 import AccManegmentSec from './AccManegmentSec'
+import { useAuth } from '@/context/AuthContext'
 
 export default function AccManegement() {
   const [currentEditSec, setCurrentEditSec] = useState("General")
-
+  const { isLightMode } = useAuth();
   return (
-    <div className='flex flex-col h-[100vh] w-[80%] mx-auto my-[3%] bg-gray-200 p-[2%] '>
+    <div className={'flex flex-col h-[100vh] w-[80%] mx-auto my-[3%] p-[2%] rounded ' + (isLightMode? "bg-gray-200" : "bg-gray-800")}>
         <div className='flex gap-4'>
-          <div className='flex flex-col gap-2 py-2 px-1 bg-[#1E3E62] text-white h-fit'>
+          <div className='flex flex-col gap-2 py-2 px-1 bg-[#134B70] text-white h-fit'>
             <section className={'accManegementBar ' + (currentEditSec === "General"? "activeAccManegementBar" : "")} onClick={()=>{
               setCurrentEditSec("General")
             }}> General</section>
