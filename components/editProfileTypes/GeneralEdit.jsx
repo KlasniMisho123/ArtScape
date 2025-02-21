fa07e7c12ea4ea5a1b5e8d3d3bc561dea95def80
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-21 17:52:02 +0400

    editProfileDetails commit

--- components/editProfileTypes/GeneralEdit.jsx
+++ components/editProfileTypes/GeneralEdit.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 export default function GeneralEdit() {
   return (
-    <div className='flex flex-col gap-2 justify-center'>
+    <div className='flex flex-col gap-4 justify-center pl-[20px]'>
       <h3 className='font-bold text-lg '> About </h3>
-      <p>Set your profile name and details.Providing additional informations 
-        like real name can help <br/> friends to find you on the ArtScape community</p>
+      <p className='text-gray-400'>Set your profile name and details.Providing additional informations 
+        like real name can help <br/> friends to find you on the ArtScape Community.</p>
+        <section>
+        <h3 className=' text-lg pb-2'>General</h3>
+        <hr className='opacity-75'/>
+        </section>
     </div>
   )
 }
