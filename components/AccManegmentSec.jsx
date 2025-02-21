743cd07dd39ec69cc32cc6591bf02dacf952732d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-21 17:39:14 +0400

    editProfileDetails commit

--- components/AccManegmentSec.jsx
+++ components/AccManegmentSec.jsx
@@ -8,7 +8,7 @@ export default function AccManegmentSec(props) {
   const {currentEditSec} = props
   return (
     <div className='flex flex-col w-[100%] h-[100vh]'>
-      <div className='flex justify-center font-bold md:text-xl sm:text-sm '> {currentEditSec} </div>
+      <div className='flex justify-center font-bold md:text-2xl sm:text-sm '> {currentEditSec} </div>
       <div className='flex flex-col justify-between mt-[40px]'>
           {currentEditSec === "General"? <GeneralEdit /> : 
           currentEditSec === "Account Details"? <AccountDetailsEdit /> :
--- components/editProfileTypes/GeneralEdit.jsx
+++ components/editProfileTypes/GeneralEdit.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function GeneralEdit() {
   return (
-    <div className='flex justify-center'>GeneralEdit</div>
+    <div className='flex flex-col gap-2 justify-center'>
+      <h3 className='font-bold text-lg '> About </h3>
+      <p>Set your profile name and details.Providing additional informations 
+        like real name can help <br/> friends to find you on the ArtScape community</p>
+    </div>
   )
 }
