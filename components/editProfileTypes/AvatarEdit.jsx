65835ec4d79c828461df48a2370572603c719656
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-23 17:33:10 +0400

    state update

--- components/editProfileTypes/AvatarEdit.jsx
+++ components/editProfileTypes/AvatarEdit.jsx
@@ -3,12 +3,15 @@ import React, { useState } from 'react'
 export default function AvatarEdit() {
   const [avatar, setAvatar] = useState("")
   return (
-    <div className='min-h-[100vh]'>
+    <div className='min-h-[100vh] flex flex-col gap-8 '>
       <div className='flex flex-col items-center gap-2 '> 
         <h3 className='font-bold text-white text-lg '> Express Yourself </h3>
         <p className='opacity-75 '>  Change Your Avatar</p>
       </div>
-      <section>
+      <section className=''>
+        <div className=''>
+          <img src='defaultProfilePicture.jpg' className=''/>
+        </div>
         <input
         type='file'
         />
