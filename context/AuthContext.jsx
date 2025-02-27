900bd3c039d7578fc83ffee42eaf53c0ae56af65
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-27 21:24:45 +0400

    auth update

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -47,14 +47,14 @@ export function AuthProvider({ children }) {
                 // if user exists, fetch data from firestore database
                 const docRef = doc(db, 'users', user.uid)
                 const docSnap = await getDoc(docRef)
-                // console.log(user.email)
+                console.log(user.email)
                 let firebaseData = {}
                 setIsAuthenticated(true)
                 if (docSnap.exists()) {
                     firebaseData = docSnap.data()
                 }
                 setUserDataObj(firebaseData)
-                console.log("userDataObj: ", userDataObj)
+                console.log("currentUser: ", firebaseData)
             } catch(err) {
                 console.log("Fetching User Err: ", err.message)
             } finally {
@@ -109,6 +109,7 @@ export function AuthProvider({ children }) {
       }, [isLightMode]);
 
     const value = {
+        userDataObj,
         isAuthenticated,
         setIsAuthenticated,
         currentUser,
