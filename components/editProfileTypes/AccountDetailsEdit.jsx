663ece99a34081a4cbeaf824ccd7fa715050a5d7
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-27 20:50:42 +0400

    user info display

--- components/editProfileTypes/AccountDetailsEdit.jsx
+++ components/editProfileTypes/AccountDetailsEdit.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 export default function AccountDetailsEdit() {
   return (
-    <div>
+    <div className=''>
       <input
       placeholder='Social Links'
       />
       <input
       placeholder='Desc'
       />
+      {/* pluss sign */}
     </div>
   )
 }
