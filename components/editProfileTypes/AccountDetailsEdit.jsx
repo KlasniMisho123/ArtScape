6383527075fba0605bdfc1ac76f8716e8208b5e5
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-28 17:50:21 +0400

    Account Details commit

--- components/editProfileTypes/AccountDetailsEdit.jsx
+++ components/editProfileTypes/AccountDetailsEdit.jsx
@@ -6,7 +6,7 @@ export default function AccountDetailsEdit() {
   const {currentUser} = useAuth()
 
   useEffect(()=>{
-    console.log(currentUser)
+    // console.log(currentUser)
   },[])
 
   return (
@@ -21,13 +21,15 @@ export default function AccountDetailsEdit() {
       </div>
       <section className='flex flex-col gap-4 shadow-2xl  '>
         <div className='text-lg font-bold bg-[#134B70] text-white px-2 py-1 '> Contact Info <i className="fa-solid fa-envelope"></i> </div>
-        <div className="flex gap-[30%]  p-2 mx-2 "> 
+        <div className="flex gap-[30%] p-2 mx-2 pb-8 "> 
           <div>
-            <p> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
+            <button className='p-1 border border-black my-1 p-1 rounded '> Change <i className="fa-solid fa-envelope"></i> </button>
+            <p className='my-1'> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
             <p> <span className='text-gray-500'> Status: </span> {currentUser?.emailVerified ? `Verified` : "Unverified"} </p>
           </div>
           <div>
-            <p> <span className='text-gray-500'> Phone: </span> <i className="fa-solid fa-mobile mx-1 "></i> {currentUser?.phoneNumber
+            <button className='p-1 border border-black my-1 p-1 rounded '> Change <i className="fa-solid fa-mobile  "> </i> </button>
+            <p className='my-1'> <span className='text-gray-500'> Phone: </span> <i className="fa-solid fa-mobile mx-1 "></i> {currentUser?.phoneNumber
              ? `Ends in: ${currentUser.phone}` : "Empty"} </p> 
             <p> <span className='text-gray-500'> Status: </span> {currentUser?.mbverify ? `Verified` : "Unverified"} </p>
           </div>
