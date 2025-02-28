4bbd33f65b6e52cd6ba4274dbcf45aa70a731591
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-28 17:12:04 +0400

    State update

--- components/editProfileTypes/AccountDetailsEdit.jsx
+++ components/editProfileTypes/AccountDetailsEdit.jsx
@@ -1,10 +1,12 @@
+"use client"
+import { useAuth } from '@/context/AuthContext'
 import React from 'react'
 
 export default function AccountDetailsEdit() {
+  const {currentUser} = useAuth()
   return (
     <div className=''>
       <div>
-        <div> Contact Info </div>
       <input
       placeholder='Social Links'
       />
@@ -12,6 +14,11 @@ export default function AccountDetailsEdit() {
       placeholder='Desc'
       />
       </div>
+      <section className='flex flex-col gap-4 shadow-2xl  '>
+        <div className='text-lg font-bold bg-[#134B70] text-white px-2 py-1 '> Contact Info <i className="fa-solid fa-envelope"></i> </div>
+        <p> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
+        <p> <span className='text-gray-500'><i className="fa-solid fa-mobile"></i>  Phone: </span> {currentUser.phone ? `Ends in: ${currentUser.phone}` : "None"} </p> 
+      </section>
       {/* pluss sign */}
     </div>
   )
