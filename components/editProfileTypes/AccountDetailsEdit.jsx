ca9d0175f815e24cfe826452a4e2a70d0f491bbb
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-28 17:18:01 +0400

    Account Details commit

--- components/editProfileTypes/AccountDetailsEdit.jsx
+++ components/editProfileTypes/AccountDetailsEdit.jsx
@@ -16,8 +16,13 @@ export default function AccountDetailsEdit() {
       </div>
       <section className='flex flex-col gap-4 shadow-2xl  '>
         <div className='text-lg font-bold bg-[#134B70] text-white px-2 py-1 '> Contact Info <i className="fa-solid fa-envelope"></i> </div>
-        <p> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
-        <p> <span className='text-gray-500'><i className="fa-solid fa-mobile"></i>  Phone: </span> {currentUser.phone ? `Ends in: ${currentUser.phone}` : "None"} </p> 
+        <div className="flex justify-between  p-2 mx-2 "> 
+          <div>
+            <p> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
+            <p> <span className='text-gray-500'> Status: </span>  </p>
+          </div>
+          <p> <span className='text-gray-500'> Phone: </span> <i className="fa-solid fa-mobile mx-1 "></i> {currentUser.phone ? `Ends in: ${currentUser.phone}` : "Empty"} </p> 
+        </div>
       </section>
       {/* pluss sign */}
     </div>
