e06656cb06dd479ffcc2cdb3f62f584318fa48a8
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-01 18:00:54 +0400

    acc details commit

--- components/editProfileTypes/AccountDetailsEdit.jsx
+++ components/editProfileTypes/AccountDetailsEdit.jsx
@@ -41,17 +41,17 @@ export default function AccountDetailsEdit() {
         <div className='text-lg font-bold bg-[#134B70] text-white px-2 py-1 '> <i class="fa-solid fa-shield mr-1"></i> Account Security  </div>
         <div className="flex gap-[30%] p-2 mx-2 pb-8 "> 
           <div>
-          <button className="p-1 border border-black my-1 px-4 rounded transition-all duration-300 hover:bg-black hover:text-white"> 
-            Change Email <i className="fa-solid fa-envelope"></i> </button>
-            <p className='my-1'> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
-            <p> <span className='text-gray-500'> Status: </span> {currentUser?.emailVerified ? `Verified` : "Unverified"} </p>
+            {/*  Two-Factor Authentication (2FA)*/}
+            <p className='my-1'>  <span className='text-gray-500'> 2FA Status: </span> Active </p> 
+            <div className='flex gap-1 '> 
+            <button className="p-1 border border-black my-1 text-red-400 px-4 rounded transition-all duration-300 hover:bg-red-500 hover:text-white"> 
+            <i class="fa-solid fa-lock"></i> Disable </button>
+            <button className="p-1 border border-black my-1 px-4 text-green-400 rounded transition-all duration-300 hover:bg-green-500 hover:text-white"> 
+            <i class="fa-solid fa-play"></i> Activate </button>
+            </div>
           </div>
           <div>
-            <button className="p-1 border border-black my-1 px-4 rounded transition-all duration-300 hover:bg-black hover:text-white"> 
-              Change Phone <i className="fa-solid fa-mobile  "> </i> </button>
-            <p className='my-1'> <span className='text-gray-500'> Phone: </span> <i className="fa-solid fa-mobile mx-1 "></i> {currentUser?.phoneNumber
-             ? `Ends in: ${currentUser.phone}` : "Empty"} </p> 
-            <p> <span className='text-gray-500'> Status: </span> {currentUser?.mbverify ? `Verified` : "Unverified"} </p>
+
           </div>
         </div>
       </section>
