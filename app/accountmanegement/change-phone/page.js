4b53b234aca3262e1fe8f2b16135a296046c537a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-04 22:41:12 +0400

    email/ number  update

---
+++ app/accountmanegement/change-phone/page.js
@@ -0,0 +1,11 @@
+import AccManegement from '@/components/AccManegement'
+import EmailEdit from '@/components/editProfileTypes/EmailEdit'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <EmailEdit />
+    </div>
+  )
+}
