3018511a0772b9fb8960513437e6605da47b9033
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-04 21:45:27 +0400

    email/ number  update

--- components/editProfileTypes/AccountDetailsEdit.jsx
+++ components/editProfileTypes/AccountDetailsEdit.jsx
@@ -5,23 +5,36 @@ import React, { useEffect, useState } from 'react'
 export default function AccountDetailsEdit() {
   const {currentUser, isLightMode} = useAuth()
   const [isF2AuthActive, setIsF2AuthActive] = useState(false)
+  const [isChangeingDigInfo, setIsChangeingDigInfo] = useState(false)
+  
 
   useEffect(()=> {
+
   },[])
 
   return (
     <div className=''>
+      {(isChangeingDigInfo === "Email" || isChangeingDigInfo === "Number") && (
+        <section>{isChangeingDigInfo}</section>
+      )}
+
       <section className='flex flex-col gap-4 shadow-2xl  '>
         <div className='text-lg font-bold bg-[#134B70] text-white px-2 py-1 '> <i className="fa-solid fa-envelope mr-1"></i> Contact Info  </div>
         <div className="flex gap-[30%] p-2 mx-2 pb-8 "> 
           <div>
-          <button className="p-1 border border-black my-1 px-4 rounded transition-all duration-300 hover:bg-black hover:text-white"> 
+          <button
+           className="p-1 border border-black my-1 px-4 rounded transition-all duration-300 hover:bg-black hover:text-white"
+           onClick={()=>{
+            setIsChangeingDigInfo("Email")
+          }}>
             Change Email <i className="fa-solid fa-envelope"></i> </button>
             <p className='my-1'> <span className='text-gray-500'> Email address: </span>{currentUser?.email || ""} </p> 
             <p> <span className='text-gray-500'> Status: </span> {currentUser?.emailVerified ? `Verified` : "Unverified"} </p>
           </div>
           <div>
-            <button className="p-1 border border-black my-1 px-4 rounded transition-all duration-300 hover:bg-black hover:text-white"> 
+            <button className="p-1 border border-black my-1 px-4 rounded transition-all duration-300 hover:bg-black hover:text-white"onClick={()=>{
+            setIsChangeingDigInfo("Number")
+            }}> 
               Change Phone <i className="fa-solid fa-mobile  "> </i> </button>
             <p className='my-1'> <span className='text-gray-500'> Phone: </span> <i className="fa-solid fa-mobile mx-1 "></i> {currentUser?.phoneNumber
              ? `Ends in: ${currentUser.phone}` : "Empty"} </p> 
