dbf4637608f9fcf5f32f162df08da8085d4d5dd7
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-06 23:13:19 +0400

    countryIndex

--- components/customHooks/useCountryFlag.js
+++ components/customHooks/useCountryFlag.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 export default function useCountryFlag(country) {
-    const [countryIndex, setCountryIndex] = useState("")
+    const [countryIndex, setCountryIndex] = useState("");
 
-    useEffect(()=>{
-        if(country==="Georgia") {
-            setCountryIndex("ge")
-        } else if(country==="Germany"){
-            setCountryIndex("de")
-        } else if(country==="France"){
-            setCountryIndex("fr")
-        } else if(country==="Italy"){
-            setCountryIndex("it")
-        } else if(country==="Spain"){
-            setCountryIndex("es")
-        } else if(country==="United Kingdom"){
-            setCountryIndex("gb")
-        } else if(country==="Poland"){
-            setCountryIndex("pl")
-        } else if(country==="Sweden"){
-            setCountryIndex("se")
-        } else {
-            setCountryIndex(country)
-        }   
-    },[])
+    const countryCodes = {
+        "Georgia": "ge",
+        "Germany": "de",
+        "France": "fr",
+        "Italy": "it",
+        "Spain": "es",
+        "United Kingdom": "gb",
+        "Poland": "pl",
+        "Sweden": "se"
+    };
 
-  return (countryIndex);
-  
+    useEffect(() => {
+        setCountryIndex(countryCodes[country] || country); 
+    }, [country]); 
+
+    return countryIndex;
 }
