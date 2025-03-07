d96e3fabddfd061c769d19992d3af083a8e162fe
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-07 17:36:42 +0400

    mini profile design

--- components/editProfileTypes/EmailEdit.jsx
+++ components/editProfileTypes/EmailEdit.jsx
@@ -1,16 +1,21 @@
 'use client'
 import { useAuth } from '@/context/AuthContext'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function EmailEdit() {
   const { currentUser } = useAuth()
+  const [hashedEmail, setHashedEmail ] = useState("")
 
-  useEffect(()=>{
+  let currentUSSer = currentUser?.email.slice()
+
+  useEffect(()=> {
     console.log("currentUser: ",currentUser)
+    
   }, [currentUser])
+
   return (
-    <div >        
-        {currentUser?.displayName}
+    <div className=''>        
+        { currentUSSer}
     </div>
   )
 }
