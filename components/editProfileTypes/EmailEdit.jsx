f249d1a8f984ea91957c8a2d76b3d7a85ee98192
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-07 23:11:45 +0400

    change email

--- components/editProfileTypes/EmailEdit.jsx
+++ components/editProfileTypes/EmailEdit.jsx
@@ -21,6 +21,10 @@ export default function EmailEdit() {
       }
   }
 
+  function clearInputs() {
+    setNewEmail("")
+  }
+
   useEffect( ()=> {
     console.log("currentUser: ",currentUser)
     hashingEmail()
@@ -28,8 +32,8 @@ export default function EmailEdit() {
 
   return (
     <div className='min-h-[75vh] w-[70%] mx-auto '>        
-        <div className='flex flex-col bg-[#134B70] mt-2 justify-center p-4 text-white  md:text-sm lg:text-md rounded '>
-            {/* <span className='cursor-pointer'> Home &gt; Account &gt; Email Preferences </span> */}
+        <div className='flex gap-2 bg-[#134B70] mt-2 items-center p-4 text-white  md:text-sm lg:text-md rounded '>
+            <i className="fa-solid fa-envelope md:text-md lg:text-lg mr-1"></i>{/* <span className='cursor-pointer'> Home &gt; Account &gt; Email Preferences </span> */}
             <h1 className='text-white sm:text-md  md:text-lg lg:text-xl '> Email Configuration</h1>
         </div>
         <div className='flex gap-4 flex-col my-10 bg-[#103d5c] rounded p-4 '> 
@@ -37,15 +41,31 @@ export default function EmailEdit() {
             <h2 className='md:text-md lg:text-lg'>Change Email For: <span> {hashedEmail} </span> </h2>
           </div>
           <div className='flex flex-col gap-2 '>
+          <div className="bg-[#1a5276] p-4 rounded text-gray-200">
+          <h3 className="text-lg">How It Works:</h3>
+            <ul className="list-disc pl-5">
+              <li>Enter your new email address.</li>
+              <li>Click "Submit" to receive a confirmation code previous Email.</li>
+              <li>Enter code in verify section.</li>
+            </ul>
+          </div>
             <label> Enter New Email </label>
             <input 
               className='border-2 border-black bg-[#243642] rounded p-2 text-white'
               value={newEmail}
               onChange={(e)=>{setNewEmail(e.target.value)}}
-              placeholder='Email'
+              placeholder='New email'
             />  
           </div>
-          
+          <div className='flex justify-end gap-16 ml-[10px] mt-[40px]'>
+          <button
+           className='rounded w-[20%] py-1 text-white bg-[#243642] shadow-lg hover:brightness-110 '
+           onClick={clearInputs}
+          > Cancel</button>
+          <button className='rounded w-[20%] py-1 text-white linear-lblue-blue shadow-lg hover:brightness-110 '
+          onClick={clearInputs}
+          > Submit </button>
+        </div>
         </div>
     </div>
   )
