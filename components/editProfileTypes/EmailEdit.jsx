761ce1621ded2c3f3ed603e5bed30921cbeaa164
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-07 22:54:45 +0400

    change email

--- components/editProfileTypes/EmailEdit.jsx
+++ components/editProfileTypes/EmailEdit.jsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react'
 export default function EmailEdit() {
   const { currentUser } = useAuth()
   const [hashedEmail, setHashedEmail ] = useState("")
-
+  const [newEmail, setNewEmail ] = useState("")
   
 
   async function hashingEmail() {
@@ -28,12 +28,24 @@ export default function EmailEdit() {
 
   return (
     <div className='min-h-[75vh] w-[70%] mx-auto '>        
-        <div className='flex flex-col bg-[#134B70]  mt-4 justify-center p-4 text-white  md:text-sm lg:text-md '>
+        <div className='flex flex-col bg-[#134B70] mt-2 justify-center p-4 text-white  md:text-sm lg:text-md rounded '>
             {/* <span className='cursor-pointer'> Home &gt; Account &gt; Email Preferences </span> */}
-            <h1 className='text-white sm:text-md  md:text-lg lg:text-xl '> Email Configuration </h1>
+            <h1 className='text-white sm:text-md  md:text-lg lg:text-xl '> Email Configuration</h1>
         </div>
-        <div className='my-10 bg-'> 
-          <h2 className='md:text-md lg:text-lg'> {hashedEmail} </h2>
+        <div className='flex gap-4 flex-col my-10 bg-[#103d5c] rounded p-4 '> 
+          <div> 
+            <h2 className='md:text-md lg:text-lg'>Change Email For: <span> {hashedEmail} </span> </h2>
+          </div>
+          <div className='flex flex-col gap-2 '>
+            <label> Enter New Email </label>
+            <input 
+              className='border-2 border-black bg-[#243642] rounded p-2 text-white'
+              value={newEmail}
+              onChange={(e)=>{setNewEmail(e.target.value)}}
+              placeholder='Email'
+            />  
+          </div>
+          
         </div>
     </div>
   )
