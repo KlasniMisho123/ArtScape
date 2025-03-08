f647a04b790b9a3261e346208562f4a85aa48819
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-08 22:56:12 +0400

    GenerateVerificationCode

--- components/editProfileTypes/PhoneEdit.jsx
+++ components/editProfileTypes/PhoneEdit.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+import { useAuth } from '@/context/AuthContext'
+import React, { useEffect, useState } from 'react'
 
 export default function PhoneEdit() {
   return (
