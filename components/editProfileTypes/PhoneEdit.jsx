c50d2377907774ca222ecfc118bd80948768fce3
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-08 23:00:27 +0400

    Phone number Change

--- components/editProfileTypes/PhoneEdit.jsx
+++ components/editProfileTypes/PhoneEdit.jsx
@@ -4,25 +4,26 @@ import React, { useEffect, useState } from 'react'
 
 export default function PhoneEdit() {
   const { currentUser } = useAuth()
-    const [hashedNumber, setHashedNumber ] = useState("")
-    const [newNumber, setNewENumber ] = useState("")
+    const [hashedPhoneNumber, setHashedPhoneNumber ] = useState("")
+    const [newPhoneNumber, setNewPhoneNumber ] = useState("")
     const [verifySection, setVerifySection ] = useState(false)
     const [verificationCode, setVerificationCode ] = useState("")
     const [generatedVerificationCode, setGeneratedVerificationCode ] = useState("")
 
-    async function hashingEmail() {
-      let userEmail = currentUser?.email
+    async function hashingPhoneNumber() {
+      let userPhoneNumber = currentUser?.email
       if(userEmail) {
           let emailFirstLetter = currentUser?.email.slice(0, 1) 
   
           let emailLastLetterIndex =  userEmail.indexOf("@")
           let emailLastLetter = userEmail.slice((emailLastLetterIndex-1), emailLastLetterIndex)
           let emailDomain = userEmail.slice((emailLastLetterIndex))
-          setHashedEmail(`${emailFirstLetter}***${emailLastLetter}${emailDomain}`)
         } else {
         }
     }
-    
+    useEffect(()=>{
+      console.log("currentUser: ",currentUser)
+    },[])
   return (
     <div>PhoneEdit</div>
   )
