4d7c2355f88ec9447303f18979cbe4ecf48bc6e8
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-09 20:17:24 +0400

    useSendEmail.js commit

--- components/customHooks/useSendEmail.js
+++ components/customHooks/useSendEmail.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
-export default function useSendEmail() {
-  return (
-    <div>useSendEmail</div>
-  )
+export default function useSendEmail(props) {
+  const { textToSend } = props
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  async function sendEmail() {
+    setIsLoading(true);
+    setError(null);
+    setIsSuccess(false);
+    try {
+      const response = await fetch('http://localhost:3000/accountmanegement/change-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(emailData), // Send the email data to the API route
+      })
+    }
+     catch(err) {
+      console.log(err.message)
+    } finally {
+
+    }
+  }
+
+  return ("")
+  
 }
