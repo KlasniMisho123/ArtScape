2154c72a8febeac4844fd706402d4a0eddace387
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-09 20:19:53 +0400

    useSendEmail.js commit

--- components/customHooks/useSendEmail.js
+++ components/customHooks/useSendEmail.js
@@ -7,20 +7,29 @@ export default function useSendEmail(props) {
   const [error, setError] = useState(null);
   const [isSuccess, setIsSuccess] = useState(false);
 
-  async function sendEmail() {
+  const sendEmail = async (emailData) => {
+    // WHY NOT async function? difference
     setIsLoading(true);
     setError(null);
     setIsSuccess(false);
+
     try {
-      const response = await fetch('http://localhost:3000/accountmanegement/change-email', {
+      const response = await fetch('http://localhost:3000/api/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(emailData), // Send the email data to the API route
-      })
-    }
-     catch(err) {
+        body: JSON.stringify(emailData),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || 'Failed to send email.');
+      }
+      
+      setIsSuccess(true);
+    } catch(err) {
       console.log(err.message)
     } finally {
 
