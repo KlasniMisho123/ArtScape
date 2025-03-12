c951f52c8899b0fe20df9f53be7a1e2f2a8a2fcb
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-12 16:50:32 +0400

    edit email

--- components/editProfileTypes/EmailEdit.jsx
+++ components/editProfileTypes/EmailEdit.jsx
@@ -73,7 +73,7 @@ export default function EmailEdit() {
     if (inputedCode === generatedVerificationCode) {
         setVerifyUserPasswordSection(true)
         
-        if (!userPassword) {
+        if (!verifyUserPassword) {
             console.log("Password is required.");
             return;
         }
