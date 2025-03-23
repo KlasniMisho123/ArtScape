e2f55a8c9258c0cdc36fd6416cc7bf6bac45a506
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-23 17:56:05 +0400

    send verification to email commit

--- components/editProfileTypes/PhoneEdit.jsx
+++ components/editProfileTypes/PhoneEdit.jsx
@@ -128,7 +128,7 @@ export default function PhoneEdit() {
     useEffect(()=>{
       console.log("currentUser: ", currentUser)
       if(currentUser) {
-        handleUserInfo
+        handleUserInfo()
       }
     },[currentUser])
 
